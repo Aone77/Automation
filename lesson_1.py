a92bb396aadfb91dbd265154ec77e611ef213f72
Aone77 <aone@keemail.me>
2022-06-15 15:10:34 +0300

    add

---
+++ lesson_1.py
@@ -0,0 +1,26 @@
+# print("Helo World!")
+# x = input()
+# # print(x)
+# x = input("Какой нибудь текст")
+# print(x)
+
+# x = int(input())
+# y = int(input())
+# print(x+y)
+# print(type(x))
+# print(y)
+
+'''первая задача'''
+x = (25)
+print(x+25)
+print(x*2)
+
+'''вторая задача'''
+# x = int(input())
+# y = int(input())
+# print(x+y)
+
+'''третья задача'''
+x=int(input())
+print(x//60)
+print (x%60)
