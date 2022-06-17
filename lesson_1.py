1ea015974348a059f009a113523d13a5e8be59a7
Aone77 <aone@keemail.me>
2022-06-17 10:39:25 +0300

    add

--- lesson_1.py
+++ lesson_1.py
@@ -1,6 +1,6 @@
 # print("Helo World!")
 # x = input()
-# # print(x)
+# print(x)
 # x = input("Какой нибудь текст")
 # print(x)
 
@@ -10,17 +10,137 @@
 # print(type(x))
 # print(y)
 
-'''первая задача'''
-x = (25)
-print(x+25)
-print(x*2)
+'''1 задача'''
+# x = (25)
+# print(x+25)
+# print(x*2)
 
-'''вторая задача'''
+'''2 задача'''
 # x = int(input())
 # y = int(input())
 # print(x+y)
 
-'''третья задача'''
-x=int(input())
-print(x//60)
-print (x%60)
+'''3 задача'''
+# x = int(input())
+# print(x // 60)
+# print(x % 60)
+
+'''4 задача'''
+# x = "abc"
+# y = "123"
+# print(x, y)
+#
+# '''5 задача'''
+# x = "Python 12345"
+# print(len(x))
+
+'''6 задача'''
+# print("Ваш текст подсказки:Яблоко")
+# a = input()
+# print("Ваш текст подсказки:Груша")
+# b = input()
+# print("Ваш текст подсказки:2")
+# с = input()
+# print(((a+b)*2))
+
+'''7 задача'''
+# a,b,c,d = False, True, False, True
+# print(a and b and not (c or d))
+
+'''8 задача'''
+# x = int(input())
+# y = int(input())
+# print(x > y)
+
+'''9 задача'''
+# x = int(input())
+# y = int(input())
+# if x+y > 10:
+#     print("Сумма больше")
+# if x+y < 10:
+#     print("Сумма меньше")
+# if x+y == 10:
+#     print("Сумма равна")
+
+'''10 задача'''
+# x = int(input())
+# if x > 0:
+#     print("1")
+# elif x < 0:
+#     print("-1")
+# else:
+#     print("0")
+
+'''11 задача'''
+# x = int(input())
+# y = int(input())
+# if x > y:
+#     print(y)
+# elif x < y:
+#     print(x)
+# else:
+#     print("Числа совпадают")
+
+'''12 задача'''
+# name = input()
+# job = input()
+# age = input()
+# print(("Имя: "+name)+"\n"+("Профессия: "+job)+"\n"+("Возраст: "+age)+"\n")
+
+'''13 задача'''
+# a, b, c, d = int(input()), int(input()), int(input()), int(input())
+# x = a+b
+# y = c+d
+# print(x/y)
+
+'''14 задача'''
+# a, b, c = int(input()), int(input()), int(input())
+# if b<a>c:
+#     print(a)
+# elif a<b>c:
+#     print(b)
+# else:
+#     print(c)
+
+'''15 задача'''
+# a, b, c = int(input()), int(input()), int(input())
+# if b>a>c or c>a>b:
+#     print(a)
+# elif a>b>c or c>b>a:
+#     print(b)
+# else:
+#     print(c)
+
+'''16 задача'''
+# x = int(input())
+# print("Следующее число для числа " ,x, "это ", x + 1,".")
+# print("Предыдущее число для числа " ,x, "это ", x - 1,".")
+
+'''17 задача'''
+# print("Сколько у Вас денег?")
+# x = int(input())
+# a = 30
+# b = 50
+# c = 100
+# y = a + b + c
+# if x > y:
+#   print("Ваша сдача: ", x - y)
+# elif x < y:
+#  print("Недостаточно денег")
+# else:
+#  print("Спасибо, что без сдачи!")
+
+'''18 задача'''
+x = int(input())
+y = int(input())
+z = int(input())
+if z >= x < y:
+    print("Это нормально")
+elif z <= x:
+    print("Слишком мало бегаете")
+else:
+    print("Много бегать вредно")
+
+
+
+
