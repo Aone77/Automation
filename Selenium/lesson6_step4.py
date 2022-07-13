e3a38a1909cd32c75b64e3477e6a4122eda46030
Aone77 <aone@keemail.me>
2022-07-13 11:42:10 +0300

    add

--- Selenium/lesson6_step4.py
+++ Selenium/lesson6_step4.py
@@ -26,34 +26,60 @@
 #     browser.quit()
 #
 # # не забываем оставить пустую строку в конце файла
+#
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import time
+# import math
+#
+#
+# link = "http://suninjuly.github.io/find_link_text"
+#
+# try:
+#     browser = webdriver.Chrome()
+#     browser.get(link)
+#     link = browser.find_element(By.PARTIAL_LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000)))
+#     link.click()
+#
+#     input1 = browser.find_element(By.TAG_NAME, "input")
+#     input1.send_keys("Ivan")
+#     input2 = browser.find_element(By.NAME, "last_name")
+#     input2.send_keys("Petrov")
+#     input3 = browser.find_element_by_class_name("city")
+#     input3.send_keys("Smolensk")
+#     input4 = browser.find_element(By.ID, "country")
+#     input4.send_keys("Russia")
+#     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+#     button.click()
+#
+# finally:
+#     # успеваем скопировать код за 30 секунд
+#     time.sleep(30)
+#     # закрываем браузер после всех манипуляций
+#     browser.quit()
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-import math
+# подготовка для теста
+# открываем страницу первого товара
+# данный сайт не существует, этот код приведен только для примера
+browser.get("https://fake-shop.com/book1.html")
 
+# добавляем товар в корзину
+add_button = browser.find_element(By.CSS_SELECTOR, ".add")
+add_button.click()
 
-link = "http://suninjuly.github.io/find_link_text"
+# открываем страницу второго товара
+browser.get("https://fake-shop.com/book2.html")
 
-try:
-    browser = webdriver.Chrome()
-    browser.get(link)
-    link = browser.find_element(By.PARTIAL_LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000)))
-    link.click()
+# добавляем товар в корзину
+add_button = browser.find_element(By.CSS_SELECTOR, ".add")
+add_button.click()
 
-    input1 = browser.find_element(By.TAG_NAME, "input")
-    input1.send_keys("Ivan")
-    input2 = browser.find_element(By.NAME, "last_name")
-    input2.send_keys("Petrov")
-    input3 = browser.find_element_by_class_name("city")
-    input3.send_keys("Smolensk")
-    input4 = browser.find_element(By.ID, "country")
-    input4.send_keys("Russia")
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-    button.click()
+# тестовый сценарий
+# открываем корзину
+browser.get("https://fake-shop.com/basket.html")
 
-finally:
-    # успеваем скопировать код за 30 секунд
-    time.sleep(30)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+# ищем все добавленные товары
+goods = browser.find_elements(By.CSS_SELECTOR, ".good")
+
+# проверяем, что количество товаров равно 2
+assert len(goods) == 2
